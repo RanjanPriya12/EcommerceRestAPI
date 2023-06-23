279d0c891ab909bfaee6a84b01bce934273279c6
Priya Ranjan <97494807+RanjanPriya12@users.noreply.github.com>
2023-06-23 23:27:28 +0530

    project folder design

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+const port = 5000 || process.env.PORT;
+
+app.listen(port,async(res,req)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+});
