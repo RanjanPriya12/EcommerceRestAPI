1f7627880f5e2fbea344220f3061cc7db9dc2ad1
Priya Ranjan <97494807+RanjanPriya12@users.noreply.github.com>
2023-06-26 09:39:30 +0530

    file upload for user profile

--- index.js
+++ index.js
@@ -2,11 +2,28 @@ const express = require("express");
 const app = express();
 const userRoute = require("./routes/user.route");
 const connectDB = require("./configs/db");
+const path = require("path");
+const cors=require('cors');
+const cloudinary = require("cloudinary");
+const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
 app.use(express.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(fileUpload());
 require("dotenv").config();
 const port = 5000 || process.env.PORT;
 
-app.use("/api/myStore",userRoute)
+app.get("/",(req,res)=>{
+    return res.send(`<h2>Welcome to our Blog-Writing App</h2>`);
+});
+
+app.use("/api/myStore",userRoute);
+cloudinary.config({ 
+    cloud_name: 'dwaguis04', 
+    api_key: '827958215438413', 
+    api_secret: 'PyhGGlW8h2-jREsWQxHDA9Qrhb0' 
+  });
 
 app.listen(port,async(res,req)=>{
     try {
