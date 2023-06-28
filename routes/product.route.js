0330b825b4256f50114d50649e9e0bf2f023b5b5
Priya Ranjan <97494807+RanjanPriya12@users.noreply.github.com>
2023-06-28 20:53:57 +0530

    product add api

--- routes/product.route.js
+++ routes/product.route.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const { isAuthorizeRoles } = require("../middlewares/authorization");
+const { createProduct } = require("../controllers/product.controller");
 const router = express.Router();
 
+router.route("/addProduct/create-product").post(isAuthenticate,isAuthorizeRoles,createProduct);
 module.exports=router;
