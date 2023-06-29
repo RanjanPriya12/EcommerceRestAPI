7f32a8ba38b0f51c88039bb74a4df38f725ff7b0
Priya Ranjan <97494807+RanjanPriya12@users.noreply.github.com>
2023-06-29 22:32:11 +0530

    get all cart items api

--- routes/cart.route.js
+++ routes/cart.route.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { addNewCartItem } = require("../controllers/cart.controller");
+const { addNewCartItem, getCartItems } = require("../controllers/cart.controller");
 const router = express.Router();
 
 router.route("/add-new-cartItem").post(addNewCartItem);
+router.route("/all-cart-items").get(getCartItems);
 
 
 module.exports=router;
