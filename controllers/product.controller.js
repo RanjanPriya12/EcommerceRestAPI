0b587940176e92b71f36a3f3d6b4f4b4331b3eda
Priya Ranjan <97494807+RanjanPriya12@users.noreply.github.com>
2023-06-30 23:16:50 +0530

    product update api

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -51,28 +51,78 @@ exports.getAllProducts = async (req, res) => {
     const products = await apiFeature.query;
     const productCount = products.length;
     const totalPages = Math.ceil(productCount / resultPerPage);
+    return res.status(200).send({
+      Success: true,
+      message: "Products fetched successfully.",
+      products,
+      productCount,
+      resultPerPage,
+      totalPages,
+    });
+  } catch (error) {
+    return res.status(500).send({ Success: false, error: error.message });
+  }
+};
+
+// get all products by admin
+exports.getProductsByAdmin = async (req, res) => {
+  try {
+    const products = await Product.find().lean().exec();
     return res
       .status(200)
       .send({
         Success: true,
         message: "Products fetched successfully.",
-        products,
-        productCount,
-        resultPerPage,
-        totalPages,
+        products: products,
       });
   } catch (error) {
     return res.status(500).send({ Success: false, error: error.message });
   }
 };
 
-
-// get all products by admin
-exports.getProductsByAdmin = async (req, res) => {
-  try {
-    const products = await Product.find().lean().exec();
-    return res.status(200).send({ Success: true,message:"Products fetched successfully.", products:products });
-  } catch (error) {
-    return res.status(500).send({ Success: false,error:error.message });
+//update product by admin
+exports.updateProduct = async (req, res) => {
+  let product = await Product.findById(req.params.id);
+  if (!product) {
+    return res
+      .status(404)
+      .send({
+        Success: false,
+        message: "Something went wrong, product not found.",
+      });
   }
+  // images cloudinary
+  let images = [];
+  if (typeof req.body.images === "string") {
+    images.push(req.body.images);
+  } else {
+    images = req.body.images;
+  }
+  if (images !== undefined) {
+    for (let i = 0; i < product.images.length; i++) {
+      await cloudinary.v2.uploader.destroy(product.images[i].public_id);
+    }
+    const imagesLinks = [];
+
+    for (let i = 0; i < images.length; i++) {
+      const result = await cloudinary.v2.uploader.upload(images[i], {
+        folder: "products",
+      });
+
+      imagesLinks.push({
+        public_id: result.public_id,
+        url: result.secure_url,
+      });
+    }
+    req.body.images = imagesLinks;
+  }
+
+  product = await Product.updateOne(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+  return res
+    .status(200)
+    .send({ success: true, product, message: "Product updated successfully." });
 };
--- routes/product.route.js
+++ routes/product.route.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const { isAuthenticate, isAuthorizeRoles } = require("../middlewares/authorization");
-const { createProduct, getAllProducts, getProductsByAdmin } = require("../controllers/product.controller");
+const { createProduct, getAllProducts, getProductsByAdmin, updateProduct } = require("../controllers/product.controller");
 
 router.route("/admin/addProduct/create-product").post(isAuthenticate,isAuthorizeRoles("admin"),createProduct);
 router.route("/").get(getAllProducts);
 router.route("admin-products").get(isAuthenticate,isAuthorizeRoles("admin"),getProductsByAdmin);
+router.route("/admin/update-product/:id").put(isAuthenticate, isAuthorizeRoles("admin"), updateProduct);
 
 module.exports=router;
